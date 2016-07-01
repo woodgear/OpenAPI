af3752b41d02af0b71e2b27422f314306d9ef265
wucong <1875486458@qq.com>
2016-07-01 16:20:53 +0800

    增加获取bing每日壁纸的标题

--- Bing/获取Bing壁纸.py
+++ Bing/获取Bing壁纸.py
@@ -1,17 +1,25 @@
 import urllib.request as re
+import urllib.parse as pr
 import re as rege
 
 
-def getBingImgsrc():
-  bing=re.urlopen("https://www.bing.com/")
-  html=str(bing.read())
-  base="https://www.bing.com"
-  pattern=rege.compile(r'g_img=\{url:\\\'([^,]*\.jpg)')
-  imagesrc= pattern.findall(html)[0]
-  return base+imagesrc
+def getImageSrc(html):
+	base="https://www.bing.com"
+	imgsrcpattern=rege.compile(r'g_img={url: "([^"]*)"')
+	imagesrc= imgsrcpattern.findall(html)[0]
+	return base+imagesrc
+
+def getImageTitle(html):
+	titlepattern=rege.compile(r'<a id="sh_cp" class="sc_light"\s*title="([^"]*)"')
+	title=titlepattern.findall(html)[0].replace('\\x','%')
+	title=pr.unquote(title)
+	return title
 
 
 
-print(getBingImgsrc())
 
-
+html=str(re.urlopen("https://www.bing.com/").read())
+imagesrc=getImageSrc(html)
+title=getImageTitle(html)
+print(imagesrc+" "+title)
+print("over")
