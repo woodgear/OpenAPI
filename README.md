7bf6c1c403e2b378c1b588d9b221df56e0bf934d
wucong <1875486458@qq.com>
2016-07-18 12:44:06 +0800

    发现一些bug 增加todo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenAPI
 常用网站获取信息的API接口
+
+##ZhiHu
+* downAllImageById.py
+使用方式 python downAllImageById.py {问题id} {保存路径}
+
--- ZhiHu/downAllImageById.py
+++ ZhiHu/downAllImageById.py
@@ -1,19 +1,18 @@
 import urllib.request as re
 import re as rege
 import os
+import sys
 import shutil
-def  getNameAndFromSrc(src):
-	name=src[src.rindex("/")+1:len(src)-4]
-	itype=src[len(src)-3:len(src)]
-	return [name,itype]
 
-def create(path):
-	if  os.path.isdir(path):
-			shutil.rmtree(path)
-	os.makedirs(path)
-	pass
+
+
 
 def downimage(src,path):
+	def  getNameAndFromSrc(src):
+		name=src[src.rindex("/")+1:len(src)-4]
+		itype=src[len(src)-3:len(src)]
+		return [name,itype]
+
 	inf=getNameAndFromSrc(src)
 	img=re.urlopen(src).read()
 	f=open(path+'/'+inf[0]+'.'+inf[1],'wb')
@@ -23,46 +22,60 @@ def downimage(src,path):
 
 
 def downAllImageById(qid,path):
+	def create(path):
+		if  os.path.isdir(path):
+				print('delete')
+				shutil.rmtree(path)
+		os.makedirs(path)
+
+	def getpage(html):
+		regexp=r'<a href="\?sort=created&amp;page=(\d*)">(\d*)</a></span>(\\n)*<span><a href="\?sort=created&amp;page=2">'
+		pattern=rege.compile(regexp)
+		page= pattern.findall(html)
+		if len(page)==0:
+			return 1
+		else:
+			return int(page[0][0])
+		
+	###############
 	create(path)
+	
 	base="https://www.zhihu.com/question/"
 	basehtml=str(re.urlopen(base+qid+'?sort=created&page=1').read())
+	print(basehtml)
 	page=getpage(basehtml)
+	
 	srcs=getAllImageSrc(base+qid+'?sort=created&page=',page)
 	for x in range(0,len(srcs)):
-
 		size=downimage(srcs[x].replace('_b','_r'),path)
-		print('下载图片 %d/%d  %.3f kb'%(x,len(srcs),size/1024),end='\r')
-		pass
+		print('下载图片 %d/%d  %.3f kb %s'%(x,len(srcs),size/1024,srcs[x]),end='\r')
 	
 
 
-def getpage(html):
-	regexp=r'<a href="\?sort=created&amp;page=(\d*)">(\d*)</a></span>(\\n)*<span><a href="\?sort=created&amp;page=2">'
-	pattern=rege.compile(regexp)
-	page= pattern.findall(html)[0][0]
-	return (int(page))
 
-def parseimagesrc(html):
 
-	pass
 
 def getAllImageSrc(link,page):
 	srcs=[]
-	iregexp=r'<img src="(https://pic\d\.zhimg\.com/[^"]*_b[^"]*)"'
+	iregexp=r'data-original="(https://pic\d\.zhimg\.com/[^"]*)"' #解析出图片的链接
 	ipattern=rege.compile(iregexp)
-	
+	page=1
 	for x in range(1,page+1):
+		print(link+str(x))
 		html=str(re.urlopen(link+str(x)).read())
 		t=ipattern.findall(html)
 		print('解析链接 %d/%d页  %d' % (x,page,len(t)),end='\r')
 		srcs+=t
 	return srcs
 
-qid='46312145'
-argv=os.argv
-if len(argv)==1:
-	downAllImageById(argv[0],argv[0])
-elif len(argv)==2:
-	downAllImageById(argv[0],argv[1])
 
+argv=sys.argv
+if len(argv)==2:
+	downAllImageById(argv[1],argv[1])
+elif len(argv)==3:
+	downAllImageById(argv[1],argv[2])
+qid='21448845'
+downAllImageById(qid,qid)
 
+#TODO 如果不登录直接访问知乎的话 会有些答案看不到
+#TODO 获取到的图片链接有可能是重复的
